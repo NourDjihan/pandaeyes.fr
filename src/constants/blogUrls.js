88b413e14e1659a69ec597a3a25140c18fad1da4
NourDjihan <a.n.djihan@gmail.com>
2021-03-10 12:51:22 +0100

    Changed the blog url

--- src/constants/blogUrls.js
+++ src/constants/blogUrls.js
@@ -1,4 +1,4 @@
-const BLOG_RAW_URL = 'https://raw.githubusercontent.com/olekscode/Blog/master';
+const BLOG_RAW_URL = 'https://raw.githubusercontent.com/NourDjihan/Blog/main';
 
 export const POSTS_FOLDER = BLOG_RAW_URL + '/posts';
 export const POSTS_METADATA_FILE = BLOG_RAW_URL + '/metadata/posts.json';
